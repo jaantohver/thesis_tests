390ff3cf2c0f72e0143b0d68f7aad1c090a28d47
Jaan Tohver <jaan.tohver@gmail.com>
2018-11-20 01:05:38 +0200

    Added CRNN folder for real this time, I hope.

---
+++ crnn/src/cpp/build/CMakeFiles/crnn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crnn.dir/init.cpp.o"
+  "CMakeFiles/crnn.dir/ctc.cpp.o"
+  "libcrnn.pdb"
+  "libcrnn.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crnn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
