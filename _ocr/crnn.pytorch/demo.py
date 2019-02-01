Correct absolute accuracy calculation. Levenshtein for relative accuracy calculation.

_ocr/crnn.pytorch/demo.py
@@ -3,6 +3,7 @@ import sys
 import torch
 import utils
 import dataset
+import numpy as np
 import models.crnn as crnn
 from PIL import Image
 from torch.autograd import Variable
@@ -35,6 +36,32 @@ transformer = dataset.resizeNormalize((100, 32))
 
 call(["rm", "-rf", "res"])
 call(["mkdir", "res"])
+
+def levenshtein(seq1, seq2):  
+    size_x = len(seq1) + 1
+    size_y = len(seq2) + 1
+    matrix = np.zeros ((size_x, size_y))
+    for x in xrange(size_x):
+        matrix [x, 0] = x
+    for y in xrange(size_y):
+        matrix [0, y] = y
+
+    for x in xrange(1, size_x):
+        for y in xrange(1, size_y):
+            if seq1[x-1] == seq2[y-1]:
+                matrix [x,y] = min(
+                    matrix[x-1, y] + 1,
+                    matrix[x-1, y-1],
+                    matrix[x, y-1] + 1
+                )
+            else:
+                matrix [x,y] = min(
+                    matrix[x-1,y] + 1,
+                    matrix[x-1,y-1] + 1,
+                    matrix[x,y-1] + 1
+                )
+    print (matrix)
+    return (matrix[size_x - 1, size_y - 1])
 
 for dirname, dirnames, filenames in os.walk(input_folder):
     if len(filenames) > 0:
@@ -83,8 +110,10 @@ for dirname, dirnames, filenames in os.walk(input_folder):
             else:
                 false_count += 1
 
+            print(levenshtein(true_label, sim_pred))
+
         if true_count is 0:
             f.write("Accuracy = 0%")
         else:
             f.write("True count = " + " " + str(true_count) + "; false count = " + str(false_count) +
-                    "; accuracy = " + str((true_count + false_count) / true_count) + "%")
+                    "; accuracy = " + str(true_count * 100 / count) + "%")