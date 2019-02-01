33828170d9e222339db2e74e4d3367ecd59acbb4
Jaan <jaan@Jaans-iMac.local>
2019-02-01 11:53:53 +0200

    Relative accuracy results.

--- _ocr/crnn.pytorch/demo.py
+++ _ocr/crnn.pytorch/demo.py
@@ -37,35 +37,37 @@ transformer = dataset.resizeNormalize((100, 32))
 call(["rm", "-rf", "res"])
 call(["mkdir", "res"])
 
-def levenshtein(seq1, seq2):  
+
+def levenshtein(seq1, seq2):
     size_x = len(seq1) + 1
     size_y = len(seq2) + 1
-    matrix = np.zeros ((size_x, size_y))
-    for x in xrange(size_x):
-        matrix [x, 0] = x
-    for y in xrange(size_y):
-        matrix [0, y] = y
+    matrix = np.zeros((size_x, size_y))
+    for x in range(size_x):
+        matrix[x, 0] = x
+    for y in range(size_y):
+        matrix[0, y] = y
 
-    for x in xrange(1, size_x):
-        for y in xrange(1, size_y):
-            if seq1[x-1] == seq2[y-1]:
-                matrix [x,y] = min(
-                    matrix[x-1, y] + 1,
-                    matrix[x-1, y-1],
-                    matrix[x, y-1] + 1
+    for x in range(1, size_x):
+        for y in range(1, size_y):
+            if seq1[x - 1] == seq2[y - 1]:
+                matrix[x, y] = min(
+                    matrix[x - 1, y] + 1,
+                    matrix[x - 1, y - 1],
+                    matrix[x, y - 1] + 1
                 )
             else:
-                matrix [x,y] = min(
-                    matrix[x-1,y] + 1,
-                    matrix[x-1,y-1] + 1,
-                    matrix[x,y-1] + 1
+                matrix[x, y] = min(
+                    matrix[x - 1, y] + 1,
+                    matrix[x - 1, y - 1] + 1,
+                    matrix[x, y - 1] + 1
                 )
-    print (matrix)
+
     return (matrix[size_x - 1, size_y - 1])
 
 for dirname, dirnames, filenames in os.walk(input_folder):
     if len(filenames) > 0:
         count = 0
+        relativeAccuracy = 0
         filenames.sort(key=lambda x: float(x.split(".")[0]))
         parts = dirname.split("/")
         subdir = parts[len(parts) - 1]
@@ -110,10 +112,15 @@ for dirname, dirnames, filenames in os.walk(input_folder):
             else:
                 false_count += 1
 
-            print(levenshtein(true_label, sim_pred))
+            levDis = levenshtein(true_label, sim_pred)
+            bigger = max(len(true_label), len(sim_pred))
+            pct = (bigger - levDis) / bigger
+            relativeAccuracy += (pct * 100) / 17
 
         if true_count is 0:
-            f.write("Accuracy = 0%")
+            f.write("Accuracy = 0%\n")
         else:
             f.write("True count = " + " " + str(true_count) + "; false count = " + str(false_count) +
-                    "; accuracy = " + str(true_count * 100 / count) + "%")
+                    "; accuracy = " + str(true_count * 100 / count) + "%\n")
+
+        f.write("Relative accuracy = " + str(relativeAccuracy) + "%\n")
