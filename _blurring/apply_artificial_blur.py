5ac4ad0b4d6e1d34eca4dae7b1ace41403c1303a
Jaan <jaan.tohver@gmail.com>
2019-02-14 20:39:38 +0200

    Another set of low res images.

--- _blurring/apply_artificial_blur.py
+++ _blurring/apply_artificial_blur.py
@@ -2,10 +2,10 @@ import cv2
 import numpy as np
 from subprocess import call
 
-call(["rm", "-rf", "res"])
-call(["mkdir", "res"])
+call(["rm", "-rf", "res_downscaled"])
+call(["mkdir", "res_downscaled"])
 
-img = cv2.imread('input.jpg')
+img = cv2.imread('doc_reg/input_downscaled.jpg')
 # cv2.imshow('Original', img)
 
 count = 1
@@ -23,8 +23,8 @@ for kernel_size in range(3, 23, 2):
     # applying the kernel to the input image
     output = cv2.filter2D(img, -1, kernel_motion_blur)
 
-    cv2.imshow('Motion Blur ' + str(kernel_size), output)
-    cv2.imwrite('res/' + str(count) + '.jpg', output)
+    # cv2.imshow('Motion Blur ' + str(kernel_size), output)
+    cv2.imwrite('res_downscaled/' + str(count) + '.jpg', output)
     count += 1
 
 # cv2.waitKey(0)
